5c3789a02e52cfbabcd62eddab2eb5db4d04f99e
Marcos Antonio dos Anjos <manjos71@gmail.com>
2016-10-07 02:22:16 -0300

    Tentativa de corrigir CNC_choiceFragment.java

--- app/src/main/java/layout/CNC_choiceFragment.java
+++ app/src/main/java/layout/CNC_choiceFragment.java
@@ -1,52 +1,97 @@
 package layout;
 
-
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.support.v7.app.AppCompatActivity;
+import android.view.*;
 import com.example.marcosantonio.cnc_machinist.R;
+import android.text.TextUtils;
+import android.widget.*;
+import java.text.DecimalFormat;
 
-//import com.example.marcosantonio.myapplication.R;
-import com.google.android.gms.plus.PlusOneButton;
+public class CNC_choiceFragment extends AppCompatActivity implements View.OnClickListener {
 
-/**
- * A fragment with a Google +1 button.
- */
-public class CNC_choiceFragment extends Fragment {
+    // Declarar variáveis
 
-    // The request code must be 0 or greater.
-    private static final int PLUS_ONE_REQUEST_CODE = 0;
-    // The URL to +1.  Must be a valid URL.
-    private final String PLUS_ONE_URL = "http://developer.android.com";
-    private PlusOneButton mPlusOneButton;
-
-
-    public CNC_choiceFragment() {
-        // Required empty public constructor
-    }
-
+    TextView txt8, txt9, txtcnv, txtcnc;
+    EditText n1, n2, n3, n4, n5;
+    Button ex, rs;
+    Double v1, v2, v3, v4, v5;
+    String cnv, cnc, hs;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_cnc_choice, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_cnc_choice);
 
-        //Find the +1 button
-        mPlusOneButton = (PlusOneButton) view.findViewById(R.id.plus_one_button);
+        // find elements
 
-        return view;
+        txt8 = (TextView) findViewById(R.id.textView8);
+        txt9 = (TextView) findViewById(R.id.textView9);
+        txtcnv = (TextView) findViewById(R.id.textView10);
+        txtcnc = (TextView) findViewById(R.id.textView11);
+
+        n1 = (EditText) findViewById(R.id.editText1);
+        n2 = (EditText) findViewById(R.id.editText2);
+        n3 = (EditText) findViewById(R.id.editText3);
+        n4 = (EditText) findViewById(R.id.editText4);
+        n5 = (EditText) findViewById(R.id.editText5);
+
+        ex = (Button) findViewById(R.id.button1);
+        rs = (Button) findViewById(R.id.button2);
+
+        ex.setOnClickListener(this);
+        rs.setOnClickListener(this);
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
+    public void onClick(View v) {
 
-        // Refresh the state of the +1 button each time the activity receives focus.
-        mPlusOneButton.initialize(PLUS_ONE_URL, PLUS_ONE_REQUEST_CODE);
+        float v1 = 0;
+        float v2 = 0;
+        float v3 = 0;
+        float v4 = 0;
+        float v5 = 0;
+        float r1 = 0;
+        float r2 = 0;
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        // check if the fields are empty
+        if (TextUtils.isEmpty(n1.getText().toString())
+                || TextUtils.isEmpty(n2.getText().toString())
+                || TextUtils.isEmpty(n3.getText().toString())
+                || TextUtils.isEmpty(n4.getText().toString())
+                || TextUtils.isEmpty(n5.getText().toString())) {
+            return;
+        }
+
+        // read EditText and fill variables with numbers
+        v1 = Float.parseFloat(n1.getText().toString());
+        v2 = Float.parseFloat(n2.getText().toString());
+        v3 = Float.parseFloat(n3.getText().toString());
+        v4 = Float.parseFloat(n4.getText().toString());
+        v5 = Float.parseFloat(n5.getText().toString());
+
+        // defines the button that has been clicked and performs the corresponding operation
+        // write operation into oper, we will use it later for output
+        switch (v.getId()) {
+            case R.id.button1:
+                r1 = (v2 + (v1 * v3));
+                r2 = (v4 + (v1 * v5));
+                txtcnv.setText("CNV" + " " + " " + df.format(r1) + "hs");
+                txtcnc.setText("CNC" + " " + " " + df.format(r2) + "hs");
+                break;
+            case R.id.button2:
+                n1.setText("");
+                n2.setText("");
+                n3.setText("");
+                n4.setText("");
+                n5.setText("");
+                txtcnv.setText("");
+                txtcnc.setText("");
+                break;
+            default:
+                break;
+        }
+
     }
-
-
 }
